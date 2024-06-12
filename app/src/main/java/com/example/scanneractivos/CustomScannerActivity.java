e2c983082f8267c6cd3c7ccde40962fa24c49ed5
luisgpm <luisgpm2@gmail.com>
2024-06-12 16:49:26 -0600

    carga de codigo

---
+++ app/src/main/java/com/example/scanneractivos/CustomScannerActivity.java
@@ -0,0 +1,5 @@
+package com.example.scanneractivos;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+public class CustomScannerActivity extends CaptureActivity  {
+}
---
+++ app/src/main/java/com/example/scanneractivos/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.scanneractivos;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+//import android.provider.MediaStore;
+
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+
+import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fabScan = findViewById(R.id.fab_scan);
+
+        fabScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Si el permiso de la cámara no está concedido, solicítalo
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            CAMERA_PERMISSION_REQUEST_CODE);
+                } else {
+                    // Si el permiso de la cámara está concedido, abre el escáner
+                    startScanner();
+                }
+            }
+        });
+
+
+
+    }
+
+    private void startScanner() {
+        IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+        integrator.setCaptureActivity(CustomScannerActivity.class); // Personaliza la actividad de escaneo
+        integrator.setOrientationLocked(false); // Permite la rotación de la pantalla
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Procesa el resultado del escaneo
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                // Si el escaneo fue cancelado
+                Toast.makeText(this, "Escaneo cancelado", Toast.LENGTH_SHORT).show();
+            } else {
+                // Si se encontró un código, puedes manejarlo aquí
+                String scannedData = result.getContents();
+                Toast.makeText(this, "Código escaneado: " + scannedData, Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // El permiso de la cámara fue otorgado, ahora puedes abrir el escáner
+                startScanner();
+            } else {
+                // El permiso de la cámara fue denegado, muestra un mensaje o toma alguna otra acción
+                Toast.makeText(this, "Permiso de la cámara denegado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+}
